da4094891610fd1afce6fd7b9237d35ac929b61d
Ribosome25 <44216320+Ribosome25@users.noreply.github.com>
2019-08-26 13:04:10 -0500

    Update log.py

--- log.py
+++ log.py
@@ -1,9 +1,9 @@
 
 def write_to_log(Str):
+    Str = str(Str)
     theTime = datetime.datetime.now().strftime('%H:%M:%S')
     try:
         with open('log.txt','a') as file:
             file.write('\n'+theTime+'\t'+Str)
     except:
         raise Warning ("Filed to save log. ")
-            
