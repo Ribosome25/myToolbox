8954e7e0c555ec16cf959357e38fd1da4e60e2ff
Ribosome25 <44216320+Ribosome25@users.noreply.github.com>
2019-10-23 14:13:17 -0500

    write ndarray to csv using pandas, in IO

--- Io.py
+++ Io.py
@@ -21,3 +21,12 @@ def mkdir(dir_name):
         print (dir_name+" already exsits.")
     finally:
         os.chdir(dir_name)
+        
+def write_csv(array,name = '_save.csv'):
+    import pandas as pd
+    assert isinstance(name,str)
+    if not name.endswith('.csv'):
+        name += '.csv'
+    
+    pd.DataFrame(array).to_csv(name)
+    return True
