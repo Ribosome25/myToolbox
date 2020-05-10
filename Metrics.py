d1ca2b60532dc89cc3fdb3ba43ff37f9530060d0
Ribosome25 <zrbbit@gmail.com>
2020-05-10 17:49:01 -0500

    4-comb

--- Metrics.py
+++ Metrics.py
@@ -7,6 +7,8 @@ Created on Mon Apr 29 14:54:19 2019
 import numpy as np
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 import copy
 #%%
 
@@ -70,7 +72,36 @@ def avg_correlation(Y_Target, Y_Predict):
     for ii in range(cols):
         rt[ii] = two_correlations(Y_Target[:,ii], Y_Predict[:, ii])[0]
     return rt.mean()
-        
+
+def corr_and_error(Y_Target, Y_Predict, multi_dimension=True, output_format=list):
+    """
+    Returns the Spearman correlation and Pearson Correlation,
+        and MSE, MAE.
+    """
+    Y_Target, Y_Predict = _check_ys(Y_Target, Y_Predict, multi_dimension)
+    scorrs, pvalue = spearmanr(Y_Target, Y_Predict)
+    pcorrs, pvalue = pearsonr(Y_Predict, Y_Target)
+    if multi_dimension:
+        scorr = scorrs
+        pcorr = pcorrs
+    else:
+        scorr = scorrs[0]
+        pcorr = pcorrs[0]
+
+    mse = mean_squared_error(Y_Target, Y_Predict)
+    mae = mean_absolute_error(Y_Target, Y_Predict)
+
+    if output_format == list:
+        return([scorr, pcorr, mse, mae])
+    elif output_format == dict:
+        return({'s-corr': scorr,
+                'p-corr': pcorr,
+                'MSE': mse,
+                'MAE': mae})
+    else:
+        print("Unknown format, to be done. return list.")
+        return([scorr, pcorr, mse, mae])
+
 def _check_ys(Y_Target, Y_Predict, multi_dimension = True):
     """Check and transform to np.array"""
     Y_Target = np.asarray(Y_Target,order = 'C',dtype=float)
@@ -81,13 +112,13 @@ def _check_ys(Y_Target, Y_Predict, multi_dimension = True):
     else:
         Y_Target = Y_Target.reshape(len(Y_Target),1)
         Y_Predict = Y_Predict.reshape(len(Y_Predict),1)
-    
+
     assert(len(Y_Target)==len(Y_Predict))
     return Y_Target, Y_Predict
-    
+
 def triplet(Y_Target, Y_Predict, multi_dimension = True, output_format = list):
     """
-    Accepts appended results in each iterations. 
+    Accepts appended results in each iterations.
     Put multi_dimension = Ture
     """
     # Y_Target = np.asarray(Y_Target,order = 'C',dtype=float)
@@ -112,7 +143,7 @@ def triplet(Y_Target, Y_Predict, multi_dimension = True, output_format = list):
     else:
         raise ValueError ('Target & Predicted are not of same length.')
         return np.nan
-    
+
     if output_format==list:
         return([nrmse, mse, pcorr])
     elif output_format==dict:
@@ -123,19 +154,19 @@ def triplet(Y_Target, Y_Predict, multi_dimension = True, output_format = list):
     else:
         print("Unknown format, to be done. return list.")
         return([nrmse,mse,pcorr])
-    
+
 def test_triplet(n_dim=3):
     if n_dim==1:
         print("TBD")
         return True
-    
+
     y1 = np.random.randint(0,19,(17,n_dim))
     y2 = np.random.randint(0,9,(17,n_dim))
     tri_nrmse, tri_mse, tri_pcorr = triplet(y1,y2,True,list)
     old_nrmse, old_mse = NRMSE(y1,y2,True)
     assert(tri_nrmse == old_nrmse)
     assert(tri_mse == old_mse)
-    
+
     rst_dict = triplet(y1,y2,True,dict)
     assert(rst_dict['NRMSE'] == old_nrmse)
     return None
@@ -143,8 +174,8 @@ def test_triplet(n_dim=3):
 def triplet_transfer(Y_Target, Y_Predict, Y_Source, multi_dimension = True, output_format = list):
     """
     Change da thang subtrac in var to the mean(Source Mean),
-    Because we don't know the mean of DomainTarget. 
-    Thus, if we predict all values as the mean of Sourse, NRMSE will be 1. 
+    Because we don't know the mean of DomainTarget.
+    Thus, if we predict all values as the mean of Sourse, NRMSE will be 1.
     """
     Y_Target, Y_Predict = _check_ys(Y_Target, Y_Predict, multi_dimension)
 
@@ -162,7 +193,7 @@ def triplet_transfer(Y_Target, Y_Predict, Y_Source, multi_dimension = True, outp
     else:
         raise ValueError ('Target & Predicted are not of same length.')
         return np.nan
-    
+
     if output_format==list:
         return([nrmse, mse, pcorr])
     elif output_format==dict:
@@ -208,11 +239,11 @@ def Accuracy(Y_Target,Y_Predict,error_rate = False,multi_dimension = False):
     else:
         raise ValueError ('Target & Predicted are not of same length.')
         return np.nan
-    
+
     return Value
 
 def F1_score(Y_Target, Y_Predict, multi_dimension = False):
-    # Y target and Y predict are forced to convert to bool. 
+    # Y target and Y predict are forced to convert to bool.
     # Only applicatble for 2-class problems.
     Y_Target = np.asarray(Y_Target,order = 'C').astype(bool)
     Y_Predict = np.asarray(Y_Predict,order = 'C').astype(bool)
@@ -222,7 +253,7 @@ def F1_score(Y_Target, Y_Predict, multi_dimension = False):
     else:
         Y_Target = Y_Target.reshape(len(Y_Target),1)
         Y_Predict = Y_Predict.reshape(len(Y_Predict),1)
-        
+
     if len(Y_Target)==len(Y_Predict):
         ...
         _true_posi = sum(Y_Target & Y_Predict)
@@ -231,21 +262,21 @@ def F1_score(Y_Target, Y_Predict, multi_dimension = False):
         _precision = _true_posi/(_true_posi + _false_posi)
         _recall = _true_posi/(_true_posi + _false_neg)
         Value = 2*_precision / (_precision + _recall)
-        
+
     else:
         raise ValueError ('Target & Predicted are not of same length.')
         return np.nan
-    
+
     return Value
-        
-        
+
+
 from sklearn.model_selection import KFold
 
 def kFold_NRMSE(Mdl,X,y,k = 5):
     kf = KFold(n_splits=k)
     X = np.asarray(X,order = 'C')
     y = np.asarray(y,order = 'C')
-    
+
     errors = []
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -254,7 +285,7 @@ def kFold_NRMSE(Mdl,X,y,k = 5):
         y_prd = Mdl.predict(X_test)
         errors.append(NRMSE(y_test,y_prd)[0])
 #        errors.append(np.corrcoef(y_test.ravel(),y_prd.ravel()))# tempera
-#        return errors # tempra 
+#        return errors # tempra
     return np.mean(errors)
 
 #%%
